b6573ab142801d084726dcee762fee32a914d14f
Priscila Martins <priscila.martins@basis.com.br>
2019-12-20 00:38:03 -0300

    Adjusts config access control

--- config/customExpress.js
+++ config/customExpress.js
@@ -7,6 +7,13 @@ module.exports = () => {
 
     app.use(bodyParser.urlencoded({extended:true}))
     app.use(bodyParser.json())
+    
+    app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+        next();
+    });      
 
     consign()
         .include('controllers')
